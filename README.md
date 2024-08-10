422f3f1793554b3534d987e27d18270f9ff8d2df
sahari <mjsahari@gmail.com>
2024-08-09 23:30:00 -0700

    feat: init dotfile repo

---
+++ README.md
@@ -0,0 +1,18 @@
+# Dotfiles
+
+Just some simple dotfiles of settings that I use on a daily basis.
+
+## Initial clone
+```bash
+git clone --recurse-submodules https://github.com/catpaladin/dotfiles.git
+```
+
+## Updating submodules
+```bash
+git submodule update --recursive --remote
+```
+
+## Setup settings with `stow`
+```bash
+./setup.sh
+```
---
+++ setup.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+# This script requires you have stow installed
+
+# make sure we have pulled in and updated any submodules
+git submodule init
+git submodule update
+
+# default linux for os specific
+if [ $(uname) = 'Darwin' ]; then
+  ALACRITTY=alacritty-darwin
+else
+  ALACRITTY=alacritty-linux
+fi
+# what directories should be installable by all users including the root user
+base=(
+    nvim
+    tmux
+    $ALACRITTY
+)
+
+# run the stow command for the passed in directory ($2) in location $1
+stowit() {
+    usr=$1
+    app=$2
+    # -v verbose
+    # -R recursive
+    # -t target
+    stow -v -R -t ${usr} ${app}
+}
+
+echo ""
+echo "Stowing apps for user: ${whoami}"
+
+# install apps available to local users and root
+for app in ${base[@]}; do
+    stowit "${HOME}" $app
+done
+
+echo ""
+echo "##### ALL DONE"
