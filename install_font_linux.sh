223f8297e8adc9fcbbfa14a4c4ccd97de049fabd
sahari <mjsahari@gmail.com>
2024-08-16 19:01:37 -0700

    feat: add noto font for tmux symbols

--- install_font_linux.sh
+++ install_font_linux.sh
@@ -8,12 +8,19 @@ else
     echo "Found fonts dir $fonts_dir"
 fi
 
-font=Hack
 version=v3.2.1
-zip=${font}.zip
-curl --fail --location --show-error https://github.com/ryanoasis/nerd-fonts/releases/download/${version}/${zip} --output ${zip}
-unzip -o -q -d ${fonts_dir} ${zip}
-rm ${zip}
+
+install_font() {
+  font=$1
+  echo "Installing ${font}"
+  zip=${font}.zip
+  curl --fail --location --show-error https://github.com/ryanoasis/nerd-fonts/releases/download/${version}/${zip} --output ${zip}
+  unzip -o -q -d ${fonts_dir} ${zip}
+  rm ${zip}
+}
+
+install_font Hack
+install_font Noto
 
 echo "fc-cache -f"
 fc-cache -f
