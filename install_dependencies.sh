2b93317dcfd204cb70b7f201f70309e9db0638bd
Mike <mjsahari@gmail.com>
2024-08-17 20:24:07 -0700

    feat: update dependency versions

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # Install nvim deps
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.54.2
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.60.1
 go install github.com/segmentio/golines@latest
 go install mvdan.cc/gofumpt@latest
-go install -v github.com/incu6us/goimports-reviser/v3@latest
+go install github.com/incu6us/goimports-reviser/v3@latest
 go install github.com/cweill/gotests/gotests@latest
 go install golang.org/x/tools/cmd/godoc@latest
-go install github.com/go-delve/delve/cmd/dlv@v1.21.0
+go install github.com/go-delve/delve/cmd/dlv@latest
 
 # Install go tools
 go install github.com/spf13/cobra-cli@latest
